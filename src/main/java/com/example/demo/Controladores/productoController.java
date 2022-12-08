767e58041424ae399af1e736918667c0dc5f59fd
Juan-Vortex <89589655+Juan-Vortex@users.noreply.github.com>
2022-12-07 19:50:12 -0500

    se agregaron los permisos de cors para mi front localhos:4200

--- src/main/java/com/example/demo/Controladores/productoController.java
+++ src/main/java/com/example/demo/Controladores/productoController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +19,7 @@ import com.example.demo.Servicios.productoService;
 // dev_JB - agregamos la etiqeuta @RequestMapping("") para identificar la ruta de los endpoint
 @RestController
 @RequestMapping("/producto")
+@CrossOrigin(origins = "http://localhost:4200/")
 public class productoController {
 
     // dev_JB - traemos los servicios con la etiqueta @Autowired
